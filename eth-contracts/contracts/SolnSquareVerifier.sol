1b0457dce15214c55e66c6d052b6755b17a59cfc
Mishal Alsharekh <mishal.sharekh@gmail.com>
2019-05-15 17:16:44 +0300

    SolutionAdded

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -9,7 +9,54 @@ import "contracts/SquareVerifier.sol";
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 // DONE 
-// contract SolnSquareVerifier is CustomERC721Token ,SquareVerifier  {
-contract SolnSquareVerifier  {
+ contract SolnSquareVerifier is CustomERC721Token {
+    // TODO define a solutions struct that can hold an index & an address
+    // DONE 
+    struct Solution{
+        uint256 tokenId; 
+        address owner;
+    }
 
-}
+    // TODO define an array of the above struct
+    // DONE 
+    Solution[] solutionsArray;
+
+    // TODO define a mapping to store unique solutions submitted
+    // DONE 
+    mapping (bytes32=>Solution)  uniqueSolutions;
+
+
+    // TODO Create an event to emit when a solution is added
+    // DONE 
+    event SolutionAdded(address owner );
+   
+   
+
+
+   // TODO Create a function to add the solutions to the array and emit the event
+    function addSolution(address _owner,uint256 _tokenId) public{
+        // to add the solutions to the array
+        // DONE 
+        Solution memory solution;
+        solution = Solution({tokenId:_tokenId,owner:_owner});
+        solutionsArray.push(solution);
+        
+        //to creat unique key
+        // uint256 timecAall;
+        // timecAall =now;
+
+
+        bytes32 key = keccak256(abi.encodePacked(_owner, _tokenId));
+
+        uniqueSolutions[key]=solution;
+
+
+        //and emit the event
+        // DONE 
+        // emit SolutionAdded( _owner );
+    }
+   
+   
+    }
+
+    
